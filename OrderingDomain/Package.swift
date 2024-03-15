17d745f3328b3fcc537d6762611cd0662155e8e6
Fernando Bustos <lufe.bura@gmail.com>
2024-03-15 15:37:19 -0600

    feat: Request business use case

--- OrderingDomain/Package.swift
+++ OrderingDomain/Package.swift
@@ -1,23 +1,31 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "OrderingDomain",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OrderingDomain",
             targets: ["OrderingDomain"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(path: "../OrderingEntities")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "OrderingDomain"),
+            name: "OrderingDomain",
+            dependencies: ["OrderingEntities"]),
         .testTarget(
             name: "OrderingDomainTests",
-            dependencies: ["OrderingDomain"]),
+            dependencies: ["OrderingDomain", "OrderingEntities"]),
     ]
 )
---
+++ OrderingDomain/Sources/UseCases/GetBusinessUseCase.swift
@@ -0,0 +1,24 @@
+//
+//  GetBusinessUseCase.swift
+//
+//
+//  Created by Luis Fernando Bustos Ramírez on 15/03/24.
+//
+
+import Foundation
+import OrderingEntities
+
+public protocol GetBusinessUseCaseType {
+    
+    func execute(completion: @escaping(Result<Business, Error>) -> Void)
+}
+
+public class GetBusinessUseCase: GetBusinessUseCaseType {
+    
+    public init() { }
+    
+    public func execute(completion: @escaping(Result<Business, Error>) -> Void) {
+        let api = BusinessAPi(service: RequestNetworkProvider())
+        api.getBusinesses(completion: completion)
+    }
+}
